c086ff84725e6a765768f65abad6d55470a78880
maxim <maxim.ryklis@spsmail.cuny.edu>
2016-10-13 08:33:48 -0400

    Create task_03.py

---
+++ task_03.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module computes earned interest"""
+
+
+import decimal
+
+PROD1 = [xrange(0, 199999), xrange(1, 15), 'Yes', decimal.Decimal(3.63) ]
+PROD2 = [xrange(0, 199999), xrange(1, 15), 'No', decimal.Decimal(4.65)]
+PROD3 = [xrange(0, 199999), xrange(16, 20), 'Yes', decimal.Decimal(4.04)]
+PROD4 = [xrange(0, 199999), xrange(16, 20), 'No', decimal.Decimal(4.98)]
+PROD5 = [xrange(0, 199999), xrange(1, 15), 'Yes', decimal.Decimal(3.63)]
+PROD6 = [xrange(0, 199999), xrange(1, 15), 'Yes', decimal.Decimal(3.63)]
+PROD7 = [xrange(0, 199999), xrange(1, 15), 'Yes', decimal.Decimal(3.63)]
+PROD8 = [xrange(0, 199999), xrange(1, 15), 'Yes', decimal.Decimal(3.63)]
+PROD9 = [xrange(0, 199999), xrange(1, 15), 'Yes', decimal.Decimal(3.63)]
+PROD10 = [xrange(0, 199999), xrange(1, 15), 'Yes', decimal.Decimal(3.63)]
+PROD11 = [xrange(0, 199999), xrange(1, 15), 'Yes', decimal.Decimal(3.63)]
+PROD12 = [xrange(0, 199999), xrange(1, 15), 'Yes', decimal.Decimal(3.63)]
+PROD13 = [xrange(0, 199999), xrange(1, 15), 'Yes', decimal.Decimal(3.63)]
